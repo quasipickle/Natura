594a39e4cb438cbba2a2747b248976f6982f1e4a
Dylan Anderson <spam@nderson.ca>
2012-03-08 15:08:51 -0700

    Updating message classes to Bootstrap classes.  Updating markup to Bootstrap standard.

--- ui/default/forgot.new.tpl.php
+++ ui/default/forgot.new.tpl.php
@@ -5,43 +5,38 @@
 </h1>
 <br />
 <?php if(isset($this->error)): ?>
-	<div class = "message error">
+	<div class = "alert alert-error">
 		<?php foreach($this->error as $error): ?>
 			<?php echo $error; ?><br />
 		<?php endforeach; ?>
 	</div>
-	<br />
 <?php endif; ?>
 <?php if($this->reset_success): ?>
-	<div class = "message success">
+	<div class = "alert alert-success">
 		<?php Lang::out('msg:password_reset'); ?>
 	</div>
 	<br />
 <?php endif; ?>
 <?php if(!$this->reset_success && !$this->load_errors): ?>
 	<form method = "post" action = "">
-		<ul class = "userform">
-			<li>
-				<label>
-					<?php Lang::out('lbl:email'); ?>
-				</label>
-				<?php echo $this->email; ?>
-			</li>
-			<li>
-				<label>
-					<?php Lang::out('lbl:new_password'); ?>
-				</label>
-				<input type = "password" name = "password" />
-			</li>
-			<li>
-				<label>
-					<?php Lang::out('lbl:confirm_password'); ?>
-				</label>
-				<input type = "password" name = "confirm_password" />
-			</li>
-			<li>
-				<input type = "submit" class = "button" name = "reset" value = "<?php Lang::outSafe('btn:reset_password'); ?>"/>
-			</li>
-		</ul>
+		<div class = "control-group">
+			<?php Lang::out('lbl:email'); ?>: 
+			<?php echo $this->email; ?>
+		</div>
+		<div class = "control-group">
+			<label>
+				<?php Lang::out('lbl:new_password'); ?>
+			</label>
+			<input type = "password" name = "password" />
+		</div>
+		<div class = "control-group">
+			<label>
+				<?php Lang::out('lbl:confirm_password'); ?>
+			</label>
+			<input type = "password" name = "confirm_password" />
+		</div>
+		<div class = "form-actions">
+			<input type = "submit" class = "btn" name = "reset" value = "<?php Lang::outSafe('btn:reset_password'); ?>"/>
+		</div>
 	</form>
 <?php endif; ?>
