a77878f426daa53dd31adc98fd95db1d31423c85
Dylan Anderson <spam@nderson.ca>
2012-03-08 15:08:12 -0700

    Adding footer & closing divs necessary for sticky footer.

--- ui/default/foot.tpl.php
+++ ui/default/foot.tpl.php
@@ -1,4 +1,9 @@
-			</div> <!-- .container -->
-		</div> <!-- .container -->
+				</div> <!-- .container -->
+			</div> <!-- #content -->
+		</div> <!-- #body -->
+		<div id = "footer">
+			<?php echo ORGANIZATION_NAME; ?> powered by <a href = "https://github.com/quasipickle/Natura">Natura</a>.
+			<span class = "pull-right">Designed with <a href = "http://twitter.github.com/bootstrap/">Bootstrap</a>.</span>
+		</div>
 	</body>
 </html>
