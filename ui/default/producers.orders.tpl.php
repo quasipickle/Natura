91ff37c0c66d08926837d3df505c3a112f209d19
Dylan Anderson <spam@nderson.ca>
2012-11-12 13:03:09 -0700

    Outputs the order as a table, rather than just a raw text dump. Removed option to download as CSV or TXT.

--- ui/default/producers.orders.tpl.php
+++ ui/default/producers.orders.tpl.php
@@ -29,10 +29,54 @@
 <?php endif; ?>
 
 <?php if(isset($this->amounts) && count($this->amounts)): ?>
-	<pre><?php include 'orders.txt.tpl.php'; ?></pre>
+	<?php Lang::out('lbl:cycle'); echo '('.date('Y-m-d',$this->cycle_start_stamp).' - '.date('Y-m-d',$this->cycle_end_stamp).')'; ?><br />
+	<?php echo $this->producer_name; ?>
+
+	<table class = "table table-condensed">
+		<thead>
+			<tr>
+				<th>
+					<?php Lang::out('lbl:product'); ?>	
+				</th>
+				<th>
+					<?php Lang::out('lbl:total'); ?>
+				</th>
+				<th>
+					<?php Lang::out('lbl:orders_individual'); ?>
+				</th>
+				<th>
+					<?php Lang::get('member'); ?>
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($this->amounts as $name=>$Item): ?>
+				<tr>
+					<th>
+						<?php echo $name; ?>
+					</th>
+					<th>
+						<?php echo $Item->total; ?>
+					</th>
+					<th colspan = "2"></th>
+				</tr>
+				<?php foreach($Item->orders as $Order): ?>
+					<tr>
+						<td colspan = "2"></td>
+						<td>
+							<?php echo $Order->count.' '.$Item->units.'@$'.number_format($Order->price,2).'/'.$Item->units; ?>
+						</td>
+						<td>
+							<?php echo $Order->member_last_name.', '.$Order->member_first_name; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endforeach; ?>		
+			</tr>
+		</tbody>
+	</table>
 	<div class = "form-actions" style = "text-align:right;">
-		<a href = "<?php echo DIR_WEB; ?>/producers/orders/?cycle=<?php echo $this->cycle_id; ?>&amp;download&amp;format=txt" class = "btn"><?php Lang::out('lbl:orders_download_txt'); ?></a>
-		<a href = "<?php echo DIR_WEB; ?>/producers/orders/?cycle=<?php echo $this->cycle_id; ?>&amp;download&amp;format=csv" class = "btn"><?php Lang::out('lbl:orders_download_csv'); ?></a>
+		<a href = "<?php echo DIR_WEB; ?>/producers/orders/?cycle=<?php echo $this->cycle_id; ?>&amp;download" class = "btn"><i class = "icon-download-alt"></i><?php Lang::out('btn:download'); ?></a>
 	</div>
 <?php elseif($this->cycle_id): ?>
 	<?php Lang::out('msg:purchase_orders_none'); ?>
