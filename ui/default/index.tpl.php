abc67cb15c753d2dc40cbb69f7664883819a3176
Dylan Anderson <spam@nderson.ca>
2012-11-12 12:58:21 -0700

    Removed hardcoded content for Stoney Creek, and instead include index.content.html, which can be edited via the interface by an admin.

--- ui/default/index.tpl.php
+++ ui/default/index.tpl.php
@@ -1,11 +1,3 @@
 <?php include 'head.tpl.php'; ?>
-<h1>
-	Welcome to Stoney Creek Natural Foods' online ordering system.
-</h1>
-<p>
-	If you would like to be involved as a member or producer, please <a href = "signup/">signup</a>.
-</p>
-<p>
-	If you have already signed up, please <a href = "login/">login</a>.
-</p>
+<?php include 'index.content.html'; ?>
 <?php include 'foot.tpl.php'; ?>
