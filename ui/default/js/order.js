808d0b5388c041f0e351f73b088294f40a756008
Dylan Anderson <spam@nderson.ca>
2012-11-12 13:16:47 -0700

    Added functionality for hiding and showing all the products for a particular producer.

--- ui/default/js/order.js
+++ ui/default/js/order.js
@@ -17,6 +17,46 @@ $(document).ready(function(){
 	$("#show-all")
 		.click(function(){
 			$(this).hide();
-			$("#products tr, #show-ordered").show();
+			$('.show-products').hide();
+			$("#products tr, #show-ordered, .hide-products").show();
+			return false;
 		});
+	
+	/* For hiding all products for a producer */
+	$(".hide-products")
+		.click(function(){
+			var $this = $(this);
+			
+			/* hide the column names */
+			$this.closest('tr').next('tr').hide();
+			
+			/* hide the products */
+			$this.closest('thead').next('tbody').children('tr').hide();
+			
+			/* Swap the buttons */
+			$this.hide();
+			$this.next('a').show();
+		});
+	$(".show-products")
+		.click(function(){
+			var $this = $(this);
+			
+			/* show the column names */
+			$this.closest('tr').next('tr').show();
+			
+			/* show the products */
+			$this.closest('thead').next('tbody').children('tr').show();
+			
+			/* Swap the buttons */
+			$this.hide();
+			$this.prev('a').show();
+		});
+	
+	/* Ensuring different input boxes for the same product auto-update when one box gets changed */
+	$("input.product").on('keyup',function(){
+		var value = $(this).val(),
+			name  = $(this).attr('name');
+		
+		$('input.product[name="'+name+'"]').val(value);
+	});
 });
